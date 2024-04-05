2645b87fd5780728e55fc73f799267c81938da71
Mark Jones <mark.jones1@sydney.edu.au>
2024-04-05 20:56:21 +0800

    WIP

--- R/data.R
+++ R/data.R
@@ -56,13 +56,10 @@ get_design <- function(N = 2500, pop_spec = NULL, idx_s = 1){
   l1 = as.numeric(l == 1)
   l2 = as.numeric(l == 2)
 
+  # joint is matrix, rows are silo, cols are knee, hip. j = 1 indicates hip
   j <- rbinom(N, 1, pop_spec$r_joint[l+1, 2])
-  # reveal for late only
-  # minic the siutation where a small number who never get revealed even if they
-  # had late-stage infection.
-  # you can leave this out, but the model spec will need to be updated
-  # because there will be linearly depenedent cols in the design matrix, e.g.
-  # l1 == er
+  # reveal for late only with negligible numbers not entering in for randomised
+  # treatment
 
   # if rand has been shut off then do not enrol any more into surgical domain
   if(all(is.na(unlist(pop_spec$r_a)))){
@@ -71,93 +68,101 @@ get_design <- function(N = 2500, pop_spec = NULL, idx_s = 1){
   } else{
     # only late silo can enter surgical domain
     er <- as.numeric(l == 1)
-    # 2% are never revealed (for whatever reason) - likely unnecessary
+    # but say 2% are never revealed in this silo (for whatever reason)
     i_rec <- as.logical(rbinom(er[l==1], 1, 0.02))
     er[l==1][i_rec] <- 0
   }
 
-
-  # randomise surgery - silo specific allocation probs.
+  # randomisation assignment for surgery - silo specific allocation probs.
   r <- rep(NA, N)
   for(i in 1:length(pop_spec$r_a)){
+    # r_a has allocation probs for early, late and chronic silo
+    # pick the current silo
     z <- pop_spec$r_a[[i]]
+    # if the elements within the silo are all non-na then
     if(all(!is.na(z))){
+      # allocate the surgical trt based on the the number of trt for the strata
       r[l==(i-1)] <- sample(0:(length(z)-1), sum(l==(i-1)), TRUE, z)
     } else {
+      # otherwise just set to zero. er will be set to zero so these will be
+      # ignored anyway
       r[l==(i-1)] <- 0
     }
   }
-  # fix those that were not randomised to control. in practice r is undef
-  # for these units
+  # fix those that are in the late silo but were not revealed, see above
   r[er == 0] <- 0
 
-  # assume clinician states a preferred surgery procedure for each pt
-  # (approx) 70% chance of clinician choosing two-stage if pt is rand to revision
-  sr <- rep(NA, N)
+  # Just take r_a_q to indicate the surgery type that actually happens
+  # as preference is not really required any more in the sims.
+  # For each silo r_a_q indicates the prob of each surg type (dair, one, two)
+  # We still will record preference for the trial.
+  dtmp <- data.table(cbind(l, er, r, srp = rep(NA, N)))
+  # For each silo.
   for(i in 1:length(pop_spec$r_a_q)){
     z <- pop_spec$r_a_q[[i]]
-    if(all(!is.na(z))){
-      # preference out of all of dair, one, two (for those rand to dair)
-      r_0 <- (r == 0) & (l == i - 1)
-      sr[r_0] <- sample(0:(length(z)-1), sum(r_0), TRUE, z)
-      # preference out of one, two (for those that were rand to rev)
-      r_1 <- (r == 1) & (l == i - 1)
-      sr[r_1] <- sample(1:(length(z)-1), sum(r_1), TRUE, z[2:length(z)]/sum(z[2:length(z)]))
-    } else {
-      sr[(l == i - 1)] <- 0
-    }
+
+    # non-randomised trt just gets whatever r_a_q implies
+    dtmp[l == i - 1 & er == 0, srp := sample(0:(length(z)-1), .N, TRUE, z)]
+    # randomised trt to dair gets dair
+    dtmp[l == i - 1 & er == 1 & r == 0, srp := 0]
+    # randomised trt to rev gets one/two based on normalised probs
+    dtmp[l == i - 1 & er == 1 & r == 1,
+         srp := sample(1:(length(z)-1), .N, TRUE, z[2:length(z)]/sum(z[2:length(z)]))]
+
   }
 
-  # pref towards two-stage, assuming revision
-  sra <- as.numeric(sr == 2)
-  # determine allocation of surgery type
-  ra <- (1-er) * sr + er * r * (sr)
+  # for kicks, set 2% of the late silo to not revealed for randomised surgery
+  # and set them to receive dair
+  # ic <- rbinom(dtmp[l == 1, .N], 1, 0.02)
+  # if(all(ic == 0)){ ic[1] <- 1}
+  # dtmp[l == 1, ic := ic]
+  # dtmp[l != 1, ic := 0]
+  # dtmp[ic == 1, `:=`(er = 0, r = 0, srp = 0)]
 
-  # 0.5% of the allocated treatments switch to a different surg type (constrain to min of 1 unit)
-  srp <- ra
-  ic <- rbinom(N, 1, 0.005)
-  if(all(ic == 0)){ ic[sample(length(ic), 1)] <- 1 } # add at least one cross over (there is always one)
-  # allows for crossover to any surgical type - these probabilities should really be silo specific
-  srp[as.logical(ic)] <- sample(0:2, sum(ic), replace = T, prob = c(0.1, 0.3, 0.6))
+  srp <- dtmp$srp
+
+  # rp is now redundant.....?
+
   # was the procedure that was performed dair or revision?
-  rp <- as.numeric(srp %in% 1:2)
+  # rp <- as.numeric(srp %in% 1:2)
 
   # reveal for duration
 
-  # reveal only occurs for the duration that the effects are being evaluated
-  # if the question of duration is answered in the cohort recv one-stage
-  # (indicated by all randomisation probs being NA for that cohort) then
-  # they no longer contribute to the randomised comparison for the duration
-  # domain.
-
-  dtmp <- data.table(rp, srp, ed = rep(0, N))
+  # Reveal only occurs for the period over which the effects are being evaluated.
+  # Once the quest has been answered we stop revealling the randomisation.
+  # Presumably we also stop randomising.
+  dtmp <- data.table(srp, ed = rep(0, N))
   if(all(!is.na(pop_spec$r_b$one))){
-    dtmp[rp == 1 & srp == 1, ed := 1]
+    dtmp[srp == 1, ed := 1]
   }
   if(all(!is.na(pop_spec$r_b$two))){
-    dtmp[rp == 1 & srp == 2, ed := 1]
+    dtmp[srp == 2, ed := 1]
   }
+
   ed <- dtmp$ed
 
   # rand to long (0), short (1) for one-stage
   # rand to short (0), long (1) for two-stage
 
-  d <- rep(NA, N)
+  dtmp <- data.table(cbind(srp, ed, d = rep(NA, N)))
+  # d <- rep(NA, N)
+  # For each surgery type
   for(i in 1:length(pop_spec$r_b)){
     z <- pop_spec$r_b[[i]]
     if(all(!is.na(z))){
-      d[srp==(i-1)] <- sample(0:(length(z)-1), sum(srp==(i-1)), TRUE, z)
+      # d[srp==(i-1)] <- sample(0:(length(z)-1), sum(srp==(i-1)), TRUE, z)
+      dtmp[srp == (i - 1), d := sample(0:(length(z)-1), .N, TRUE, z)]
     } else {
-      d[srp==(i-1)] <- 0
+      dtmp[srp == (i - 1), d := 0]
     }
   }
 
+  d <- dtmp$d
+
   # reveal for ab choice
 
-  # again, reveal only occurs for the duration that the effects are being
-  # evaluated if the question of choice is decided
-  # (indicated by all randomisation probs being NA for rif) then
-  # they no further enrolments happen for ab choice.
+  # Again, reveal only occurs for the period that effects are being
+  # evaluated. Once the question is answered we reveal no more pts to rand trt.
 
   if(all(is.na(pop_spec$r_c))){
     ef <- rep(0, N)
@@ -172,9 +177,12 @@ get_design <- function(N = 2500, pop_spec = NULL, idx_s = 1){
     l, l1, l2, j,
     er, ed, ef,
     erx = 1-er, edx = 1-ed, efx=1-ef,
-    r, sr, sra, ra,
-    ic, rp,
+    r,
+    # sr, sra, ra,
+    # ic,
+    # rp,
     srp,
+    srp0 = as.numeric(srp == 0),
     srp1 = as.numeric(srp == 1),
     srp2 = as.numeric(srp == 2),
     d,
@@ -235,14 +243,28 @@ get_trial_data <- function(
 
       eta <- a0 +
         m["l1"] * d$l1 + m["l2"] * d$l2 +
-        b["erx"] * d$erx +
-        (b["r1"] * d$r + b["r2"] * d$r * d$srp2) * d$er +
-        b["edx"] * d$edx +
+        (b["erx-r0"] * d$srp0 + b["erx-r1"] * d$srp1 + b["erx-r2"] * d$srp2) * d$erx +
+        # move to separation of effects for duration based on
+        # one-stage and two-stage rather than a linear combination of
+        # terms as was done earlier on in develoment.
+        # this will necessitate an update to the decision rules
+        # and possibly other elements such as simulation par settings
+        (b["r1"] * d$srp1 + b["r2"] * d$srp2) * d$r * d$er +
+
+        # exclude this for now as leads to an estimation problem due to
+        # collinearity in the data
+        # b["edx"] * d$edx +
 
         # move to separation of effects for duration based on
         # one-stage and two-stage rather than a linear combination of
         # terms as was done earlier on in develoment.
-        (b["r1d"] * d$rp * d$d * d$srp1 + b["r2d"] * d$rp * d$d * d$srp2) * d$ed +
+
+        # 2024-04-03 - think that the d$rp is not required because if
+        # srp1 = 1 or srp2 = 1 then revision occurred otherwise dair occurred
+        # so you do not need rp.
+        # (b["r1d"] * d$rp * d$d * d$srp1 + b["r2d"] * d$rp * d$d * d$srp2) * d$ed +
+
+        (b["r1d"] * d$d * d$srp1 + b["r2d"] * d$d * d$srp2) * d$ed +
         b["efx"] * d$efx +
         b["f"] * d$f * d$ef
 
@@ -284,18 +306,18 @@ get_trial_data <- function(
 get_stan_data <- function(d){
 
   d_s <- d[, .(y = sum(y), n = .N),
-           keyby = .(l1, l2, er, ed, ef, r, rp, srp, srp2, d, f)]
+           keyby = .(l1, l2, er, r, srp, srp0, srp1, srp2, ed, d, ef, f)]
 
   ld <- list(
     N = nrow(d_s), y = d_s$y, n = d_s$n,
     l1 = d_s$l1, l2 = d_s$l2,
     er = d_s$er, ed = d_s$ed, ef = d_s$ef,
     r = d_s$r, d = d_s$d, f = d_s$f,
-    rp = d_s$rp,
+    srp0 = as.numeric(d_s$srp == 0),
     srp1 = as.numeric(d_s$srp == 1),
     srp2 = as.numeric(d_s$srp == 2),
     pri_m_sd = rep(1, 2),
-    pri_b_sd = rep(1, 8),
+    pri_b_sd = rep(1, 9),
     prior_only = 0
   )
 
@@ -324,7 +346,7 @@ test_get_design <- function(){
   pop_spec <- get_pop_spec()
   d1 <- get_design(N = 10000, pop_spec)
   stopifnot("no surgery reveal" = d1[er == 1, .N] > 0)
-  stopifnot("no duration reveal (one-stage)" = d1[ed == 1 & srp2 == 0, .N] > 0)
+  stopifnot("no duration reveal (one-stage)" = d1[ed == 1 & srp1 == 1, .N] > 0)
   stopifnot("no duration reveal (two-stage)" = d1[ed == 1 & srp2 == 1, .N] > 0)
   stopifnot("no surgery reveal" = d1[ef == 1, .N] > 0)
 
@@ -334,7 +356,7 @@ test_get_design <- function(){
   pop_spec$r_a$late['rev'] <- NA
   d1 <- get_design(N = 10000, pop_spec)
   stopifnot("no surgery reveal" = d1[er == 1, .N] == 0)
-  stopifnot("no duration reveal (one-stage)" = d1[ed == 1 & srp2 == 0, .N] > 0)
+  stopifnot("no duration reveal (one-stage)" = d1[ed == 1 & srp1 == 1, .N] > 0)
   stopifnot("no duration reveal (two-stage)" = d1[ed == 1 & srp2 == 1, .N] > 0)
   stopifnot("no surgery reveal" = d1[ef == 1, .N] > 0)
 
@@ -346,7 +368,7 @@ test_get_design <- function(){
   pop_spec$r_b$one['short'] <- NA
   d1 <- get_design(N = 10000, pop_spec)
   stopifnot("no surgery reveal" = d1[er == 1, .N] > 0)
-  stopifnot("no duration reveal (one-stage)" = d1[ed == 1 & srp2 == 0, .N] == 0)
+  stopifnot("no duration reveal (one-stage)" = d1[ed == 1 & srp1 == 1, .N] == 0)
   stopifnot("no duration reveal (two-stage)" = d1[ed == 1 & srp2 == 1, .N] > 0)
   stopifnot("no surgery reveal" = d1[ef == 1, .N] > 0)
 
@@ -356,7 +378,7 @@ test_get_design <- function(){
   pop_spec$r_b$two['short'] <- NA
   d1 <- get_design(N = 10000, pop_spec)
   stopifnot("no surgery reveal" = d1[er == 1, .N] > 0)
-  stopifnot("no duration reveal (one-stage)" = d1[ed == 1 & srp2 == 0, .N] > 0)
+  stopifnot("no duration reveal (one-stage)" = d1[ed == 1 & srp1 == 1, .N] > 0)
   stopifnot("no duration reveal (two-stage)" = d1[ed == 1 & srp2 == 1, .N] == 0)
   stopifnot("no surgery reveal" = d1[ef == 1, .N] > 0)
 
@@ -368,7 +390,7 @@ test_get_design <- function(){
   pop_spec$r_b$two['short'] <- NA
   d1 <- get_design(N = 10000, pop_spec)
   stopifnot("no surgery reveal" = d1[er == 1, .N] > 0)
-  stopifnot("no duration reveal (one-stage)" = d1[ed == 1 & srp2 == 0, .N] == 0)
+  stopifnot("no duration reveal (one-stage)" = d1[ed == 1 & srp1 == 1, .N] == 0)
   stopifnot("no duration reveal (two-stage)" = d1[ed == 1 & srp2 == 1, .N] == 0)
   stopifnot("no surgery reveal" = d1[ef == 1, .N] > 0)
 
@@ -378,7 +400,7 @@ test_get_design <- function(){
   pop_spec$r_c['rif'] <- NA
   d1 <- get_design(N = 10000, pop_spec)
   stopifnot("no surgery reveal" = d1[er == 1, .N] > 0)
-  stopifnot("no duration reveal (one-stage)" = d1[ed == 1 & srp2 == 0, .N] > 0)
+  stopifnot("no duration reveal (one-stage)" = d1[ed == 1 & srp1 == 1, .N] > 0)
   stopifnot("no duration reveal (two-stage)" = d1[ed == 1 & srp2 == 1, .N] > 0)
   stopifnot("no surgery reveal" = d1[ef == 1, .N] == 0)
 
@@ -392,7 +414,7 @@ test_get_design <- function(){
   pop_spec$r_b$two['short'] <- NA
   d1 <- get_design(N = 10000, pop_spec)
   stopifnot("no surgery reveal" = d1[er == 1, .N] == 0)
-  stopifnot("no duration reveal (one-stage)" = d1[ed == 1 & srp2 == 0, .N] == 0)
+  stopifnot("no duration reveal (one-stage)" = d1[ed == 1 & srp1 == 1, .N] == 0)
   stopifnot("no duration reveal (two-stage)" = d1[ed == 1 & srp2 == 1, .N] == 0)
   stopifnot("no surgery reveal" = d1[ef == 1, .N] > 0)
 
@@ -404,7 +426,7 @@ test_get_design <- function(){
   pop_spec$r_c['rif'] <- NA
   d1 <- get_design(N = 10000, pop_spec)
   stopifnot("no surgery reveal" = d1[er == 1, .N] == 0)
-  stopifnot("no duration reveal (one-stage)" = d1[ed == 1 & srp2 == 0, .N] > 0)
+  stopifnot("no duration reveal (one-stage)" = d1[ed == 1 & srp1 == 1, .N] > 0)
   stopifnot("no duration reveal (two-stage)" = d1[ed == 1 & srp2 == 1, .N] > 0)
   stopifnot("no surgery reveal" = d1[ef == 1, .N] == 0)
 
@@ -418,7 +440,7 @@ test_get_design <- function(){
   pop_spec$r_c['rif'] <- NA
   d1 <- get_design(N = 10000, pop_spec)
   stopifnot("no surgery reveal" = d1[er == 1, .N] > 0)
-  stopifnot("no duration reveal (one-stage)" = d1[ed == 1 & srp2 == 0, .N] == 0)
+  stopifnot("no duration reveal (one-stage)" = d1[ed == 1 & srp1 == 1, .N] == 0)
   stopifnot("no duration reveal (two-stage)" = d1[ed == 1 & srp2 == 1, .N] == 0)
   stopifnot("no surgery reveal" = d1[ef == 1, .N] == 0)
 
